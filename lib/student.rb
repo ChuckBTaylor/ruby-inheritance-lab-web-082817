ded5bd3e99390a54f3384cd0cdf3444e474dff7b
ChuckBTaylor <WLScripps@gmail.com>
2017-09-01 14:53:45 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+
+  
+  def initialize
+    super
+    @knowledge = []
+  end
+
+  def learn(knew_knowledge)
+    self.knowledge << knew_knowledge
+  end
 
 end
